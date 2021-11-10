be3ec87e35a9058ecb41a763c02d204155342f0e
Natalia Sobisz <nsobisz@INF.UG.EDU.PL>
2021-11-10 11:20:23 +0100

    skrócenie kodu dla 4 pierwszych testów

--- main.py
+++ main.py
@@ -56,18 +56,11 @@ class HammingTest(unittest.TestCase):
 
 class hamming:
     def distance(self, str1, str2):
-        if str1=="" and str2=="":
+        if str1==str2:
             return 0
-
         elif len(str1)==1 and len(str2)==1:
-            if str1==str2:
-                return 0
-            else:
                 return 1
         elif len(str1)>1 and len(str2)>1:
-            if str1==str2:
-                return 0
-            else:
                 suma=0
                 for i in range(len(str1)):
                     if str1[i]!=str2[i]:
