5fd6c0d57dfa98cf20c61632df30319de25d5e04
Natalia Sobisz <nsobisz@INF.UG.EDU.PL>
2021-11-10 10:57:05 +0100

    test na dwóch pustych str

---
+++ main.py
@@ -0,0 +1,60 @@
+import unittest
+
+class HammingTest(unittest.TestCase):
+
+    def test_empty_strands(self):
+        self.assertEqual(hamming().distance("", ""), 0)
+
+    @unittest.skip("ignore")
+    def test_single_letter_identical_strands(self):
+        self.assertEqual(hamming.distance("A", "A"), 0)
+
+    @unittest.skip("ignore")
+    def test_single_letter_different_strands(self):
+        self.assertEqual(hamming.distance("G", "T"), 1)
+
+    @unittest.skip("ignore")
+    def test_long_identical_strands(self):
+        self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
+
+    @unittest.skip("ignore")
+    def test_long_different_strands(self):
+        self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+
+    @unittest.skip("ignore")
+    def test_disallow_first_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("AATG", "AAA")
+
+    @unittest.skip("ignore")
+    def test_disallow_second_strand_longer(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("ATA", "AGTG")
+
+    @unittest.skip("ignore")
+    def test_disallow_left_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("", "G")
+
+    @unittest.skip("ignore")
+    def test_disallow_right_empty_strand(self):
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("G", "")
+
+    # Utility functions
+
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
+
+
+
+class hamming:
+    def distance(self, str1, str2):
+        if str1=="" and str2=="":
+            return 0
