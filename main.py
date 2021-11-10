b0426fb7ed6085203192342fc1e701e627fd2480
Natalia Sobisz <nsobisz@INF.UG.EDU.PL>
2021-11-10 11:09:01 +0100

    jedna litera poprawa literowki

--- main.py
+++ main.py
@@ -59,8 +59,10 @@ class hamming:
         if str1=="" and str2=="":
             return 0
 
-        elif len(str1)==1 and len(str1)==1:
+        elif len(str1)==1 and len(str2)==1:
             if str1==str2:
                 return 0
             else:
                 return 1
+
+
