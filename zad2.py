9e5ccaa5a6d645d76425895404389f8a478c5448
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 01:11:33 +0100

    zad2 test dla 1-3

--- zad2.py
+++ zad2.py
@@ -2,7 +2,11 @@
 def roman(liczba):
     liczby_rzymskie = {"I" : 1 }
     wynik=""
-    if liczba==1:
-        wynik+="I"
+    if liczba <= 3:
+        while liczba != 0:
+            for i in liczby_rzymskie:
+                if liczba >= liczby_rzymskie[i]:
+                    liczba = liczba - liczby_rzymskie[i]
+                    wynik += i
 
     return wynik
