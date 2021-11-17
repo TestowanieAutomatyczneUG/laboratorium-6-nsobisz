6f72429e8c9442469724e297d2c1381e4687ca90
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 01:23:41 +0100

    zad2 test dla 1-39

--- zad2.py
+++ zad2.py
@@ -1,12 +1,14 @@
 
 def roman(liczba):
-    liczby_rzymskie = {"IX": 9, "V": 5,"IV": 4, "I" : 1 }
+    liczby_rzymskie = {"X": 10, "IX": 9, "V": 5,"IV": 4, "I" : 1 }
     wynik=""
-    if liczba <= 9:
+    if liczba <= 39:
         while liczba != 0:
             for i in liczby_rzymskie:
                 if liczba >= liczby_rzymskie[i]:
                     liczba = liczba - liczby_rzymskie[i]
                     wynik += i
+                    break
 
     return wynik
+
