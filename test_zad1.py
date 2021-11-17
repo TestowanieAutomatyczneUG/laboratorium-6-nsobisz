439dfae6ab33a2c6c9648ee03ab06749496238a0
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 01:55:50 +0100

    zad1 test dla pojedynczych liter

--- test_zad1.py
+++ test_zad1.py
@@ -8,11 +8,11 @@ class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(hamming().distance("", ""), 0)
 
-    @unittest.skip
+
     def test_single_letter_identical_strands(self):
         self.assertEqual(hamming().distance("A", "A"), 0)
 
-    @unittest.skip
+
     def test_single_letter_different_strands(self):
         self.assertEqual(hamming().distance("G", "T"), 1)
 
--- zad1.py
+++ zad1.py
@@ -2,3 +2,9 @@ class hamming:
     def distance(self, str1, str2):
         if str1=="" and str2=="":
             return 0
+
+        elif len(str1)==1 and len(str2)==1:
+            if str1==str2:
+                return 0
+            else:
+                return 1
