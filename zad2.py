26b94c01d0125104754f3a58447795ea9464295e
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 01:34:12 +0100

    zad2 test dla 400-499

--- zad2.py
+++ zad2.py
@@ -1,8 +1,8 @@
 
 def roman(liczba):
-    liczby_rzymskie = {"C": 100,"XC": 90, "L": 50,"XL": 40, "X": 10, "IX": 9, "V": 5,"IV": 4, "I" : 1 }
+    liczby_rzymskie = {"CD": 400,"C": 100,"XC": 90, "L": 50,"XL": 40, "X": 10, "IX": 9, "V": 5,"IV": 4, "I" : 1 }
     wynik=""
-    if liczba <= 399:
+    if liczba <= 499:
         while liczba != 0:
             for i in liczby_rzymskie:
                 if liczba >= liczby_rzymskie[i]:
