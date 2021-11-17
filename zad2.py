6bd729d78b664819d87828009b563f545a062b4b
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 01:38:01 +0100

    zad2 wszystkie testy

--- zad2.py
+++ zad2.py
@@ -1,14 +1,14 @@
 
 def roman(liczba):
-    liczby_rzymskie = {"CM": 900, "D": 500, "CD": 400,"C": 100,"XC": 90, "L": 50,"XL": 40, "X": 10, "IX": 9, "V": 5,"IV": 4, "I" : 1 }
+    liczby_rzymskie = {"M": 1000, "CM": 900, "D": 500, "CD": 400,"C": 100,"XC": 90, "L": 50,"XL": 40, "X": 10, "IX": 9, "V": 5,"IV": 4, "I" : 1 }
     wynik=""
-    if liczba <= 999:
-        while liczba != 0:
-            for i in liczby_rzymskie:
-                if liczba >= liczby_rzymskie[i]:
-                    liczba = liczba - liczby_rzymskie[i]
-                    wynik += i
-                    break
+
+    while liczba != 0:
+        for i in liczby_rzymskie:
+            if liczba >= liczby_rzymskie[i]:
+                liczba = liczba - liczby_rzymskie[i]
+                wynik += i
+                break
 
     return wynik
 
