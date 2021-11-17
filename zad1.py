e06a95379dd859dc2d3b6e80e1c3c9600b1a57e9
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 02:04:30 +0100

    zad1 lepszy program

--- zad1.py
+++ zad1.py
@@ -1,25 +1,14 @@
 class hamming:
     def distance(self, str1, str2):
-        if len(str1)!="" and len(str2)=="":
-            raise ValueError("pierwszy str nie jest pusty, drugi jest")
-        elif len(str1)=="" and len(str2)!="":
-            raise ValueError("pierwszy str jest pusty, drugi nie")
-        elif len(str1)>len(str2):
-            raise ValueError("pierwszy str jest dłuższy")
-        elif len(str1)<len(str2):
-            raise ValueError("drugi str jest dłuższy")
-        elif str1=="" and str2=="":
+        if len(str1)!=len(str2):
+            raise ValueError("Str nie są równej długości")
+        elif str1==str2:
             return 0
-
         elif len(str1)==1 and len(str2)==1:
-            if str1==str2:
-                return 0
-            else:
                 return 1
-
-        else:
-            suma=0
-            for i in range(len(str1)):
-                if str1[i]!=str2[i]:
-                    suma+=1
-            return suma
+        elif len(str1)>1 and len(str2)>1:
+                suma=0
+                for i in range(len(str1)):
+                    if str1[i]!=str2[i]:
+                        suma+=1
+                return suma
