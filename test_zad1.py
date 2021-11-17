4c9fe79d62a7088546b4fe105316275bcd0c56f5
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 02:00:29 +0100

    zad1 wyjątek dla dłuższego str1

--- test_zad1.py
+++ test_zad1.py
@@ -24,7 +24,6 @@ class HammingTest(unittest.TestCase):
     def test_long_different_strands(self):
         self.assertEqual(hamming().distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
-    @unittest.skip
     def test_disallow_first_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming().distance("AATG", "AAA")
--- zad1.py
+++ zad1.py
@@ -1,6 +1,8 @@
 class hamming:
     def distance(self, str1, str2):
-        if str1=="" and str2=="":
+        if len(str1)>len(str2):
+            raise ValueError("pierwszy str jest dłuższy")
+        elif str1=="" and str2=="":
             return 0
 
         elif len(str1)==1 and len(str2)==1:
