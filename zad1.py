ca8926e613b69050423d52107e88645bbc6de881
Natalia Sobisz <nata.so@wp.pl>
2021-11-17 10:12:01 +0100

    testowanie ci

--- zad1.py
+++ zad1.py
@@ -2,7 +2,7 @@ class hamming:
     def distance(self, str1, str2):
         if len(str1)!=len(str2):
             raise ValueError("Str nie są równej długości")
-        elif str1==str2:
+        elif str1!=str2:
             return 0
         elif len(str1) == 1 and len(str2)==1:
                 return 1
